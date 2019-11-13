47579b45194361ff1c019d7bbd92595c9dd7a603
Artem <stalker3343@gmail.com>
2019-11-13 15:18:29 +0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <img
+      src="https://media.giphy.com/media/zBL9j9oiR3VM4/giphy.gif"
+      class="uk-position-center"
+      alt
+    />
+  </div>
+</template>
+<script>
+export default {
+  middleware: ["auth"]
+};
+</script>
